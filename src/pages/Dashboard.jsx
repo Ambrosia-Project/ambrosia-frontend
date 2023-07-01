33d7933309847adbc4a5c5e2e93ec8e5709c09ec
Burak Keçeci <bburakkececi@gmail.com>
2023-07-01 16:45:28 +0300

    table added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,19 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import StarIcon from '@mui/icons-material/Star'
 import { Box, Button, Container, CssBaseline, Divider, Grid, Paper, Typography } from '@mui/material'
-import React from 'react'
-import spagetthi from '../assets/images/spagetthi.jpg'
-import calamari from '../assets/images/calamari.png'
-import cake from '../assets/images/cake.png'
-import caesar from '../assets/images/caesar.png'
-import chef from '../assets/images/chef.png'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import SendIcon from '@mui/icons-material/Send';
-import StarIcon from '@mui/icons-material/Star';
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import * as React from 'react'
 import Carousel from 'react-material-ui-carousel'
+import caesar from '../assets/images/caesar.png'
+import cake from '../assets/images/cake.png'
+import calamari from '../assets/images/calamari.png'
+import chef from '../assets/images/chef.png'
+import spagetthi from '../assets/images/spagetthi.jpg'
 
 export default function Dashboard() {
 
@@ -18,7 +23,7 @@ export default function Dashboard() {
         <Paper elevation={3} sx={{ margin: '5% 7%', }}>
           <Grid container spacing={2} sx={{ padding: '2% 7%', maxHeight: '1200px' }}>
             <Grid item xs={12} md={12}>
-              <Box style={{ display:'flex', justifyContent:'flex-start', alignItems: 'center' }}>
+              <Box style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
                 <img src={chef} alt='chef' style={{ width: '80px', height: '80px', borderRadius: '10px' }} />
                 <Typography variant="h4" component="div" >
                   Chief's Advice
@@ -36,11 +41,9 @@ export default function Dashboard() {
           </Grid>
         </Paper>
 
-
-
-
+        {/* // most popular */}
         <Grid container spacing={2} sx={{ padding: '1% 7%' }}>
-          <Grid item xs={12} md={8}>
+          <Grid item xs={12} md={7}>
             <Paper elevation={1} sx={{ padding: '3% 7%', height: '100%' }}>
 
               <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
@@ -65,26 +68,39 @@ export default function Dashboard() {
               </Grid>
             </Paper>
           </Grid>
-          <Grid item xs={12} md={4}>
+
+          {/* // meals in the order */}
+          <Grid item xs={12} md={5}>
             <Paper elevation={1} sx={{ padding: '3% 7%' }}>
-              <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-                Recently Ordered
+              <Typography variant="h5" component="div" sx={{ flexGrow: 1, paddingTop: '3%' }}>
+                Meals In The Order
               </Typography>
               <Divider sx={{ margin: '10px 0' }} />
-              <Grid container spacing={2} sx={{ padding: '3% 7% 3% 0' }}>
-                <Grid item xs={12} md={12}>
-                  <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-                    Spagetthi Bolognese
-                  </Typography>
-                  <Typography variant="body1" component="div" sx={{ flexGrow: 1 }}>
-                    Amount: 2
-                  </Typography>
-                  <Typography variant="body1" component="div" sx={{ flexGrow: 1 }}>
-                    Price: 25 $
-                  </Typography>
-                  <Button variant="contained" color='success' endIcon={<ShoppingCartIcon />} sx={{ marginTop: '20px' }}>Order Again</Button>
-                </Grid>
-              </Grid>
+              <TableContainer>
+                <Table aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell align='center'>Order Number</TableCell>
+                      <TableCell align="center">Meal</TableCell>
+                      <TableCell align="center">Status</TableCell>
+                      <TableCell align="center">Price($)</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow
+                        key={row.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell align='center'>{row.id}</TableCell>
+                        <TableCell align="center">{row.name}</TableCell>
+                        <TableCell align="center"><div style={{background: '#E7B10A', padding:'1px 10px', borderRadius:'10px', textAlign:'center'}}><p style={{color:'#fff'}}>preparing</p></div></TableCell>
+                        <TableCell align="center">{row.price}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </Paper>
           </Grid>
         </Grid>
@@ -92,3 +108,16 @@ export default function Dashboard() {
     </CssBaseline>
   )
 }
+
+const rows = [
+  {
+    id: '1',
+    name: 'Spagetthi Bolognese',
+    price: 10.0,
+  },
+  {
+    id: '2',
+    name: 'Spagetthi Bolognese',
+    price: 10.0,
+  },
+]
