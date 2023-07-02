a23aad12ceb51557df867cd6c6eae6a58a32dc54
Burak Keçeci <bburakkececi@gmail.com>
2023-07-02 10:10:31 +0300

    blog details loading added

--- src/pages/BlogDetailsPage.jsx
+++ src/pages/BlogDetailsPage.jsx
@@ -1,64 +1,84 @@
-import { Box, CssBaseline, Grid, Paper, Typography } from "@mui/material";
+import { Box, CircularProgress, Container, CssBaseline, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
 import blogService from "../services/blog.service";
 
 export default function BlogDetailsPage() {
   const [blog, setBlog] = React.useState({});
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
+    setLoading(true);
     const id = window.location.pathname.split("/")[2];
     blogService.getBlogById(id).then((res) => {
       setBlog(res?.data);
       console.log(res);
+      setLoading(false);
     });
   }, []);
 
   return (
     <CssBaseline>
-      <Paper elevation={3} sx={{ margin: "3%", padding: "5%" }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} md={12}>
-            <Typography variant="h4" component="h2" noWrap>
-              {blog.title}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={12} md={12}>
-            <Typography variant="p" component="p" sx={{ color: "grey" }}>
-              {"By " + blog.name}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={12} md={12}>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                marginTop: "3%",
-              }}
-            >
-              <img
-                src={blog.image}
-                alt={blog.title}
-                style={{
-                  width: "60%",
-                  height: "60%",
-                  borderRadius: "10px",
-                  margin: "0 auto",
-                }}
-              />
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={12} md={12}>
-            <Typography
-              variant="body1"
-              component="p"
-              sx={{ wordWrap: "break-word" }}
-            >
-              {blog.content}
-            </Typography>
-          </Grid>
-        </Grid>
-      </Paper>
+      <Container>
+        {
+          loading ? (<Container
+            style={{
+              height: "100vh",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "#5E714E",
+              fontFamily: "Poppins",
+            }}
+          >
+            <CircularProgress color="inherit" />
+          </Container>
+          ) : (<Paper elevation={3} sx={{ margin: "3%", padding: "5%" }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={12} md={12}>
+                <Typography variant="h4" component="h2" noWrap>
+                  {blog.title}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={12} md={12}>
+                <Typography variant="p" component="p" sx={{ color: "grey" }}>
+                  {"By " + blog.name}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={12} md={12}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: "3%",
+                  }}
+                >
+                  <img
+                    src={blog.image}
+                    alt={blog.title}
+                    style={{
+                      width: "60%",
+                      height: "60%",
+                      borderRadius: "10px",
+                      margin: "0 auto",
+                    }}
+                  />
+                </Box>
+              </Grid>
+              <Grid item xs={12} sm={12} md={12}>
+                <Typography
+                  variant="body1"
+                  component="p"
+                  sx={{ wordWrap: "break-word" }}
+                >
+                  {blog.content}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+          )}
+      </Container>
     </CssBaseline>
   );
 }
